86d40fc28ecfb25930a3782d2eefd08f8a5e01ea
augusto2052008 <augustovdavilar@gmail.com>
2025-06-11 08:45:58 -0500

    Model Cuidad

--- ejemplo00/app1/admin.py
+++ ejemplo00/app1/admin.py
@@ -3,7 +3,8 @@ from django.contrib import admin
 # Register your models here.
 # Importar las clases del modelo
 from app1.models import Estudiante
+from app1.models import Cuidad
 
 admin.site.register(Estudiante)
-
+admin.site.register(Cuidad)
 
